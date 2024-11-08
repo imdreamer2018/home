c988fca0a945211663ab393347139c1f24f6895d
yangqian <qian.yang2@thoughtworks.com>
2024-11-08 15:07:19 +0800

    fix

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,12 +3,6 @@
     <Transition name="fade" mode="out-in">
       <div v-if="!store.playerState || !store.playerLrcShow" class="power">
         <!-- 以下信息请不要修改哦 -->
-        <span class="hidden">
-          &amp;&nbsp;Made&nbsp;by
-          <a :href="config.github" target="_blank">
-            {{ config.author }}
-          </a>
-        </span>
         <!-- 站点备案 -->
         <a v-if="siteIcp" href="https://beian.miit.gov.cn" target="_blank">
           &amp;
